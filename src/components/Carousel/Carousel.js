2eceb6d12c321d916939d9eda5b1cb8e60fd2c15
Rhodan Cervantes <105742277+rhodanrsc@users.noreply.github.com>
2023-03-08 20:27:50 -0700

    Made carousel reusable, added git links and some styling

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,36 +1,36 @@
 import React from "react";
 import info from '../../Info.json'
 import { CCarousel, CCarouselCaption, CCarouselItem, CImage } from "@coreui/react"
+import { IoLogoGithub } from "react-icons/io5";
 import landingPage from './images/CCLandingPage.png'
 import Forum from './images/CCForum.png'
 import portfolio from './images/portfolio.png'
 import '@coreui/coreui/dist/css/coreui.min.css'
 
+
+
 const Carousel = () => {
     return (
         <div className="Carousel">
             <CCarousel controls indicators>
-            <CCarouselItem>
-                <CImage className="d-block w-100 slide" align="center" src={landingPage} alt="Clean Collective - Landing Page" />
-                <CCarouselCaption className="d-none d-md-block carouselCaption">
-                <h5>Clean Collective - Landing Page</h5>
-                <p>The landing page of Clean Collective</p>
-                </CCarouselCaption>
-            </CCarouselItem>
-            <CCarouselItem>
-                <CImage className="d-block w-100 slide" align="center" src={Forum} alt="slide 2" />
-                <CCarouselCaption className="d-none d-md-block carouselCaption">
-                <h5>Clean Collective - Forum Page</h5>
-                <p>Page where Clean Tech companies can engage in discussions</p>
-                </CCarouselCaption>
-            </CCarouselItem>
-            <CCarouselItem>
-                <CImage className="d-block w-100 slide" align="center" src={portfolio} alt="slide 3" />
-                <CCarouselCaption className="d-none d-md-block carouselCaption">
-                <h5>Portfolio</h5>
-                <p>My personal portfolio to showcase my skills</p>
-                </CCarouselCaption>
-            </CCarouselItem>
+                {info.projects.map((proj) => {
+                    return(
+                        <CCarouselItem>
+                            <CImage className="d-block w-100 slide" 
+                            align="center" 
+                            src={proj.img === "landing" ? landingPage :
+                                proj.img === "forum" ? Forum :
+                                proj.img === "portfolio" ? portfolio : null} 
+                            alt={proj.alt} />
+                            <CCarouselCaption className="d-none d-md-block carouselCaption">
+                                <h5>{proj.title}</h5>
+                                <p>{proj.desc}</p>
+                                <p id="projectStack">{proj.stack}</p>
+                                <a id="projGit" href={proj.code} target="_blank"><IoLogoGithub /></a>
+                            </CCarouselCaption>
+                        </CCarouselItem>
+                    )
+                })}
             </CCarousel>
         </div>
     );
